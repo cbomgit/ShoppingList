11fa4a5c240933056f2065ef116f44ed55ed5b43
Christian Boman <christianbboman@gmail.com>
2013-05-02 18:33:51 -0700

    renamed project and packages to reflect actual app name

---
+++ src/com/christian/shoppingList/Item.java
@@ -0,0 +1,28 @@
+package com.christian.shoppingList;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Item {
+
+	
+	private String             name;				//name of the item
+	private String[] 	       location;			//where item was purchased
+	private Integer            quantity;			//how many in stock (optional)
+	private int            	   status;				//in stock/out of stock etc
+	private String             datePurchased;		//date purchased
+	
+	//private String message; short description of item, ie. on sale, buy extra, check price before, etc
+	
+	public static final int    IN_STOCK = 0;
+	public static final int    OUT = 1;
+	public static final int    LOW = 2;
+	
+	public static final int    DEFAULT_NUM_LOCATIONS = 3;
+	
+	public Item(String itemName, String itemLocation, Integer howMany) {
+		
+	}
+	
+	
+}
---
+++ src/com/christian/shoppingList/ShoppingListFragment.java
@@ -0,0 +1,33 @@
+package com.christian.shoppingList;
+
+import com.christian.grocerylist.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class ShoppingListFragment extends Fragment {
+
+	private EditText searchBar;
+	private ListView shoppingList;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.main_list_fragment,
+				container, false);
+		
+		searchBar = (EditText) rootView.findViewById(R.id.searchBar);
+		shoppingList = (ListView) rootView.findViewById(R.id.masterListView);
+		
+		shoppingList.requestFocus();
+		
+		return rootView;
+	}	
+}
